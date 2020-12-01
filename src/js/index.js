aeba93c1b6d7a26882f110b2995f12227ad97d8f
Aleksandr <Skyline_hr_33@outlook.com>
2020-12-01 23:15:37 +0200

    added initial layout

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// dependencies layer
+import 'normalize.css';
+import "../../node_modules/@fortawesome/fontawesome-free/css/all.css";
+// developers styles layer
+import "../css/global.scss";
+import "../css/layout.scss";
+import "../css/mixins.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,61 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "/src/js/index.js",
-    mode: "development",
-    output: {
-      path: path.resolve(__dirname, '/dist'),
-      filename: 'build.js',
-      publicPath: '/' // public URL of the output directory when referenced in a browser
-    },
-    module: {
-        rules: [ 
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "/src/pages/index.html"),
-            inject: 'body'
-        })
+  entry: "/src/js/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "build.js",
+    publicPath: "/", // public URL of the output directory when referenced in a browser
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, '/src/pages'),
-        open: true,
-        compress: true,
-        port: 8080,
-    } 
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/pages/index.html"),
+      inject: "body",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "./src/pages"),
+    open: true,
+    compress: true,
+    port: 8080,
+  },
+};
