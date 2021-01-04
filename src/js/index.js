f89f87c523b387e7d4e25b985e1ac3e3c59b2071
Aleksandr <Skyline_hr_33@outlook.com>
2021-01-04 03:13:17 +0200

    added slider title

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,5 @@ import "../../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../css/global.scss";
 import "../css/layout.scss";
 import "../css/mixins.scss";
+// images
+// import "../../dist/68a7ac0daab835e85a6c462729bdd940.jpg";
